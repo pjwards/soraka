30772e8027a937f33ba014bdf2d27340c17809ff
Donghyun <egaoneko@naver.com>
2018-06-26 02:34:08 +0900

    feat: Add auth

--- App/backend/routes/api/auth/index.js
+++ App/backend/routes/api/auth/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   register,
   login,
+  check,
 } from './auth.controller';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/check', check);
 
 export default router;
