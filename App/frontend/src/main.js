be4fd9aad4a4725fc4809d67e0d541a1b802c56d
Donghyun <egaoneko@naver.com>
2018-06-29 01:15:30 +0900

    feat: Add facebook sdk

--- App/frontend/src/main.js
+++ App/frontend/src/main.js
@@ -12,6 +12,31 @@ Vue.prototype.$http = axios;
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App,
+  },
   template: '<App/>',
 });
+
+// load facebook sdk
+window.fbAsyncInit = function fbAsyncInit() {
+  window.FB.init({
+    appId: '1621617961401149',
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: 'v3.0',
+  });
+};
+
+(function loadFacebookSDK(d, s, id) {
+  const fjs = d.getElementsByTagName(s)[0];
+
+  if (d.getElementById(id)) {
+    return;
+  }
+
+  const js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://connect.facebook.net/ko_KR/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
